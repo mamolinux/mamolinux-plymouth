6bf3282f7afe3ba44a34877519e8f7102a5752aa
Steve Langasek <steve.langasek@ubuntu.com>
2012-04-11 04:59:54 +0000

    [PATCH] [main] Don't watch for keyboard input if no keyboard
    Gbp-Pq: main-Don-t-watch-for-keyboard-input-if-no-keyboard.patch.

--- src/main.c
+++ src/main.c
@@ -1451,7 +1451,8 @@ start_boot_splash (state_t    *state,
       return NULL;
     }
 
-  ply_keyboard_watch_for_input (state->keyboard);
+  if (state->keyboard != NULL)
+    ply_keyboard_watch_for_input (state->keyboard);
 
   update_display (state);
   return splash;
