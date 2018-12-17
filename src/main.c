8cf88ced0d1f9752ea0cb0ff3144d7f7712c9364
Sebastien Bacher <seb128@ubuntu.com>
2018-12-17 20:18:03 +0100

    [PATCH] device-manager: don't watch for udev events when deactivated
    Gbp-Pq: 0013-device-manager-don-t-watch-for-udev-events-when-deac.patch.

--- src/main.c
+++ src/main.c
@@ -1337,6 +1337,7 @@ on_deactivate (state_t       *state,
         ply_trace ("deactivating");
         cancel_pending_delayed_show (state);
 
+        ply_device_manager_pause (state->device_manager);
         ply_device_manager_deactivate_keyboards (state->device_manager);
 
         if (state->boot_splash != NULL) {
@@ -1371,6 +1372,8 @@ on_reactivate (state_t *state)
         ply_device_manager_activate_keyboards (state->device_manager);
         if (state->boot_splash && ply_boot_splash_uses_pixel_displays (state->boot_splash))
                 ply_device_manager_activate_renderers (state->device_manager);
+
+        ply_device_manager_unpause (state->device_manager);
 
         state->is_inactive = false;
 
