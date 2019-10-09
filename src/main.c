c9e6178d06d31b6ae5a5b501ccb9afb4fba25728
Sebastien Bacher <seb128@debian.org>
2019-10-09 17:23:40 +0200

    Set default DeviceTimeout to 5.0, otherwise boot fails,
    Gbp-Pq: ubuntu-default-devicetimeout.patch.

--- src/main.c
+++ src/main.c
@@ -2273,6 +2273,10 @@ main (int    argc,
         find_system_default_splash (&state);
         find_distribution_default_splash (&state);
 
+        /* Device timeout may not be NAN or zero */
+        if (isnan (state.device_timeout) || state.device_timeout <= 0.0)
+                state.device_timeout = 5.0;
+
         if (ply_kernel_command_line_has_argument ("plymouth.ignore-serial-consoles"))
                 device_manager_flags |= PLY_DEVICE_MANAGER_FLAGS_IGNORE_SERIAL_CONSOLES;
 
