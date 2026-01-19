b9da92f71bdc6802b257b3a1de7561152790a538
Simon Johnsson <simon.johnsson@canonical.com>
2026-01-19 15:42:10 +0100

    Undocumented changes
    Gbp-Pq: misc-changes.patch.

--- src/libply-splash-core/ply-renderer.h
+++ src/libply-splash-core/ply-renderer.h
@@ -69,6 +69,8 @@ ply_renderer_type_t ply_renderer_get_type (ply_renderer_t *renderer);
 ply_list_t *ply_renderer_get_heads (ply_renderer_t *renderer);
 ply_pixel_buffer_t *ply_renderer_get_buffer_for_head (ply_renderer_t      *renderer,
                                                       ply_renderer_head_t *head);
+unsigned int ply_renderer_get_bits_per_pixel_for_head (ply_renderer_t      *renderer,
+                                                       ply_renderer_head_t *head);
 
 void ply_renderer_flush_head (ply_renderer_t      *renderer,
                               ply_renderer_head_t *head);
