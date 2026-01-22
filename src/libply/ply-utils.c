0b04b9e31133b6d04dd9cb109edf8f3070e520e4
Alessandro Astone <alessandro.astone@canonical.com>
2026-01-22 14:48:08 +0100

    [PATCH] two-step: Add support for fsck status messages
    Gbp-Pq: ubuntu-spinner-fsck.patch.

--- src/libply/ply-utils.c
+++ src/libply/ply-utils.c
@@ -333,7 +333,7 @@ ply_write_uint32 (int      fd,
         return ply_write (fd, buffer, 4 * sizeof(uint8_t));
 }
 
-static ssize_t
+ssize_t
 ply_read_some_bytes (int    fd,
                      void  *buffer,
                      size_t max_bytes)
@@ -342,6 +342,7 @@ ply_read_some_bytes (int    fd,
         size_t total_bytes_read = 0;
 
         assert (fd >= 0);
+        assert (buffer != NULL);
 
         bytes_left_to_read = max_bytes;
 
